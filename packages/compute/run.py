9fd38282680b3e76081d94581d5e58de1f80d88f
Rushad <mohammedrushad00@gmail.com>
2025-03-28 15:04:53 +0100

    testing filepath out

--- packages/compute/run.py
+++ packages/compute/run.py
@@ -76,7 +76,8 @@ def main():
     filepath_in = json.loads(os.environ["FILEPATH"])
     print("filepath_in: ", filepath_in)
     filepath_out = run_dataset_action(command, filepath_in)
-    # print_output({"filepath": filepath_out})
+    print_output({"filepath": filepath_out})
+    filepath_out = run_dataset_action(command, filepath_in)
 
 
 
